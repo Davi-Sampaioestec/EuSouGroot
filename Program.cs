b992ab3426ee32e864d3ac3717f191bf6137e4a9
Davi-sampaioestec <davisampaioall@gmail.coom>
2025-06-24 19:40:49 -0300

    Tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Eu sou o Groot. (digite 'tchau' para encerrar).");
+       
+     string pergunta;
+       
+    do
+    {
+        Console.Write("\nVocê: ");
+        pergunta = Console.ReadLine();
+        Console.WriteLine(pergunta.ToLower() != "tchau" ? "Groot: Eu sou Groot!" : "");
+           
+    } while (pergunta.ToLower() != "tchau");
+       
+ Console.WriteLine("Groot: Eu sou Groot... (saindo)");
+
